0e705a38d0aaf79ea776ddc23e9840a4931f6f65
aaron.sun <aaron.sun@rock-chips.com>
2023-12-05 18:22:14 +0800

    drivers: mpp_osal: add api for rockit
    Signed-off-by: aaron.sun <aaron.sun@rock-chips.com>
    Change-Id: I2f945004f199b61935a4a1f5a49cf2d1c1ec093b
    (cherry picked from commit e882c3db936ae295d007457f34b7e8fa0209d028)

--- drivers/video/rockchip/mpp_osal/mpp_osal.c
+++ drivers/video/rockchip/mpp_osal/mpp_osal.c
@@ -4,6 +4,9 @@
  *
  */
 #include "mpp_osal.h"
+#include <linux/platform_device.h>
+#include "base.h"
+#include "power/power.h"
 
 struct device_node *mpp_dev_of_node(struct device *dev)
 {
@@ -28,3 +31,15 @@ int mpp_device_init_wakeup(struct device *dev, bool enable)
 	return device_init_wakeup(dev, enable);
 }
 EXPORT_SYMBOL(mpp_device_init_wakeup);
+
+void osal_device_add_driver(void *dev, void *drv)
+{
+#ifdef CONFIG_PM_SLEEP
+	struct device *kdev = (struct device *)dev;
+	struct platform_driver *mpi_driver = (struct platform_driver *)drv;
+
+	kdev->driver = &mpi_driver->driver;
+	kdev->power.no_pm_callbacks = 0;
+#endif
+}
+EXPORT_SYMBOL(osal_device_add_driver);
